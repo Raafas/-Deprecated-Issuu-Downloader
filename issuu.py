fe3b5eaed0a4806f95b9cb8fdfe096da3e6599b6
Rafael Leandro <rafaelleandro@me.com>
2015-10-11 08:39:18 -0300

    Download test

--- issuu.py
+++ issuu.py
@@ -1,5 +1,5 @@
-import urllib, re
-import sys, getopt
+import urllib, wget
+import sys, getopt, re
 
 
 base = "http://issuu.com/marcosnogueira0/docs/the_legend_of_zelda_oracle_of_seaso"
@@ -8,9 +8,10 @@ pageNum = 1
 
 if len(sys.argv) > 1:
     base = str(sys.argv[1])
+    # resp = urllib.urlopen(Request(base, headers={'User-Agent': 'Mozilla'}))
     resp = urllib.urlopen(base)
     content = resp.read()
-    with open("foo.txt", "r+") as f:
+    with open("foo.txt", "w") as f:
         f.write(content)
         f.close()
     with open('foo.txt') as f:
@@ -19,7 +20,7 @@ if len(sys.argv) > 1:
                 break
     line = line.replace('<link rel="image_src" href="', '')
     line = line.replace('1.jpg">', '')
-    print line
+    file_name = wget.download(line.strip()+str(pageNum)+".jpg")
 
 
 else:
